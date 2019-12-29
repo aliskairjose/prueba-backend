5f16575ecf3606a40bba207eaac560843ac02351
Kervin Gonzalez <kervingonzalez@gmail.com>
2019-12-29 16:37:25 -0400

    nvio de correo

---
+++ resources/views/emails/notification.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Recovery Password</title>
+</head>
+<body>
+
+    <h2>Recovery Password Mail</h2>
+    <div>
+        Hi!, we send you a mail to recovery your password!.
+    </div>
+</body>
+</html>
---
+++ resources/views/emails/recovery-password.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Recuperación de clave</title>
+</head>
+<body>
+
+    <h2>Recuperción de clave de usuario</h2>
+
+    <p>Has recibido un correo para recuperar tu clave de usuario!</p>
+
+</body>
+</html>
