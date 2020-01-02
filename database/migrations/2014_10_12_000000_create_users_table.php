3b154ec407056439b0a26a6999085ba0246a8f00
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-01-02 13:39:58 -0400

    ajustes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,8 +24,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
-            $table->timestamps('created_at');
-            $table->timestamps('updated_at');
+            $table->timestamps();
         });
     }
 
