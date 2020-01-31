d72b7dc3fa5bdbda6b3b7868a3eeb769337b1daf
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-01-31 18:21:35 -0400

    Landing Controller, model y resource

---
+++ app/Http/Resources/LandingCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class LandingCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
