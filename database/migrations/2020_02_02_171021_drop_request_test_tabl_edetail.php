21ed194cd8f9a7379df2c568bca9ce522c12024d
milevisj <milevisj@gmail.com>
2020-02-02 12:17:49 -0500

    drop request tests fix

--- database/migrations/2020_02_02_171021_drop_request_test_tabl_edetail.php
+++ database/migrations/2020_02_02_171021_drop_request_test_tabl_edetail.php
@@ -14,7 +14,7 @@ class DropRequestTestTablEdetail extends Migration
     public function up()
     {
         //
-        Schema::drop('request_test');
+        Schema::drop('request_tests');
     }
 
     /**
