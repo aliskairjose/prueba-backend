32146a2d8f1e8cfaa8b91b0d0dfbe81db91bf967
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-02-04 12:27:50 -0400

    Logica de monedero

---
+++ app/RetreatRequest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class RetreatRequest extends Model
+{
+    use Notifiable;
+
+    protected $fillable = [ ];
+}
