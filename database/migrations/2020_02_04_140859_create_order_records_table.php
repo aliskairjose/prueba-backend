c24fb18ac6e20f1ef29b29e8f10032aabe4fad1d
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-02-04 10:24:06 -0400

    create table history orders

---
+++ database/migrations/2020_02_04_140859_create_order_records_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('history_records', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('order_id')->index('order_id')->nullable(false);
+            $table->unsignedInteger('user_id')->nullable(false);
+            $table->string('status', 100)->nullable(false);
+            $table->foreign('order_id')->references('id')->on('my_orders');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_records');
+    }
+}
