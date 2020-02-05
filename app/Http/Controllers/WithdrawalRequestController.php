03c90bef7f2fac4a35b02f4d950d2e2a9a2cdf6a
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-02-05 08:23:27 -0400

    Validacion de monto y disponibilidad

--- app/Http/Controllers/WithdrawalRequestController.php
+++ app/Http/Controllers/WithdrawalRequestController.php
@@ -8,6 +8,7 @@ use App\WithdrawalRequest;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class WithdrawalRequestController extends Controller
 {
@@ -69,6 +70,23 @@ class WithdrawalRequestController extends Controller
     public function store(Request $request)
     {
         try {
+
+            $user =$this->getAuthenticatedUser();
+
+            $wallet = $this->$user->wallet;
+
+            // Valida que el usuario tenga saldo suficiente en la wallet
+            if ($request-amount > $wallet->amount) {
+                return response()->json(
+                  [
+                    'isSuccess' => false,
+                    'message'   => 'No posee saldo suficiente en la wallet',
+                    'status'    => 400,
+                  ]
+                );
+            }
+
+
             $data = WithdrawalRequest::create(
               [
                 'amount'  => $request->amount,
@@ -129,4 +147,19 @@ class WithdrawalRequestController extends Controller
         );
     }
 
+    private function getAuthenticatedUser()
+    {
+        try {
+            if (!$user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+            return response()->json(['token_expired']);
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+            return response()->json(['token_invalid']);
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+            return response()->json(['token_absent']);
+        }
+        return $user;
+    }
 }
