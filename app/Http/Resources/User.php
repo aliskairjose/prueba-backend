260f2176b128a11679fe32e912a8058ea46ed269
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-02-05 22:47:51 -0400

    ajuste de loguep

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -15,20 +15,21 @@ class User extends JsonResource
     public function toArray($request)
     {
 
+
+
         return [
           'id'                   => $this->id,
           'name'                 => $this->name,
           'surname'              => $this->surname,
           'email'                => $this->email,
           'birthday'             => $this->birthday,
-          'type_user'            => 'dropshiper', //quitar esto
+          'type_user'            => $this->role->name,
           'status'               => $this->status,
           'register_approved'    => $this->register_approved,
           'approve_product'      => $this->approve_product,
           'banned'               => $this->banned,
           'role_id'              => $this->role_id,
           'role'                 => $this->role,
-//          'subscription_plan_id' => $this->subscription_plan_id,
           'subscription_plan'    => new SubscriptionPlan($this->plan),
           'products'             => new ProductCollection($this->products),
           'wallet'               => isset($this->wallet->amount) ? $this->wallet->amount : 0.00,
