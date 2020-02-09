e0ffed437e31588fb32c7de243b50f1149ea64e0
milevisj <milevisj@gmail.com>
2020-02-09 13:50:31 -0500

    as

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::post('/landing/url', 'LandingPageController@getByUrl');
-
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 
 Route::post('login', 'Api\AuthController@login');
 Route::post('register', 'Api\RegisterController@register');
@@ -160,7 +157,7 @@ Route::group(['middleware' => 'jwt.verify'], function () {
     // LandingPage Page Routes
     Route::get('landing', 'LandingPageController@index');
     Route::post('landing', 'LandingPageController@store');
- 
+    Route::post('landing/url', 'LandingPageController@getByUrl');
     Route::put('landing/{id}', 'LandingPageController@update');
     Route::delete('landing/{id}', 'LandingPageController@delete');
 
