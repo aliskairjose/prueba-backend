6fdd0c10dffb76bacf2906618c55ef18bb4b90dd
milevisj <milevisj@gmail.com>
2020-02-09 13:49:36 -0500

    asas

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::post('/landing/url', 'LandingPageController@getByUrl');
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/api/landing/url', 'LandingPageController@getByUrl');
 
 Route::post('login', 'Api\AuthController@login');
 Route::post('register', 'Api\RegisterController@register');
