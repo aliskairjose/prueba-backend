8e2e5227b842dcdc816b38e1b2ea2a5852e72349
fernando <fernandoga22@gmail.com>
2020-02-10 10:00:08 -0500

    servientrega trayectorias

--- app/Imports/TrajectoriesImport.php
+++ app/Imports/TrajectoriesImport.php
@@ -4,17 +4,11 @@ namespace App\Imports;
 
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
-use App\Department;
-use App\City;
-use App\Trajectory;
 
 class TrajectoriesImport implements ToCollection
 {
 
 
-    /**
-     * @param Collection $collection
-     */
     public function collection(Collection $rows)
     {
         var_dump($rows);
