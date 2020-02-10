ee9633712a7610088cc6a98c49cdc02fb914d0ce
fernando <fernandoga22@gmail.com>
2020-02-10 11:04:23 -0500

    servientrega trayectorias

--- app/Imports/TrajectoriesImport.php
+++ app/Imports/TrajectoriesImport.php
@@ -23,7 +23,6 @@ class TrajectoriesImport implements ToCollection
     public function collection(Collection $collection)
     {
         $cont = 0;
-        var_dump($cont);
         foreach ($collection as $row) {
             if ($cont > 0) {
 
