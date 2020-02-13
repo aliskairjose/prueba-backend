75a6ab02d7cd3c0480e6bdf8c86b8ad5ffe612f8
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-02-13 09:40:16 -0400

    ajuste

--- app/Http/Controllers/MyOrderController.php
+++ app/Http/Controllers/MyOrderController.php
@@ -102,7 +102,7 @@ class MyOrderController extends Controller
                     [
                         'isSuccess' => true,
                         'status'    => 400,
-                        'message'   => 'No puede solicitar nuevamente muestras de este producto a este suplier'
+                        'message'   => 'No puede solicitar nuevamente muestras de este producto a este supp lier'
                     ]
                 );
             }
